e6f2567b318a93658e027f5016a891e2eb8a08ed
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2026-08-10 16:04:13 -0300

    xd

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -5,8 +5,8 @@ console.log('Configurando Nodemailer para SMTP Gmail...');
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: NODEMAILER_USER,
     pass: NODEMAILER_PASSWORD,
@@ -14,9 +14,6 @@ const transporter = nodemailer.createTransport({
   connectionTimeout: 30000,
   greetingTimeout: 15000,
   socketTimeout: 30000,
-  tls: {
-    rejectUnauthorized: false,
-  },
 });
 
 // Verificacion SMTP opcional:
